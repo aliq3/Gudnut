f2ab86b1dd578231461b2465860d4f051c25b6e7
Muhammad Nurkhaliq <muhammadkhaliq@student.untan.ac.id>
2019-09-21 22:54:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gudnut/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.gudnut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText pas,usr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usr = (EditText) findViewById(R.id.username);
+        pas = (EditText) findViewById(R.id.password);
+    }
+
+    public void loginbtn(View view) {
+        String user = usr.getText().toString();
+        String pass = pas.getText().toString();
+
+        background bg = new background(this);
+        bg.execute(user, pass);
+
+    }
+}
